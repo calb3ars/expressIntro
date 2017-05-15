ea55fab1dda808e472d7ce4c2224149070cc0b1e
Andrew Jiang <andrew@Andrews-MacBook-Pro.local>
2017-05-15 00:15:56 -0700

    Server runs and implement write and end methods

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response) {
+  response.write('Hello world');
+  response.end();
+});
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
+}
+
+);
